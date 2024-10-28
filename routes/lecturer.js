6dcb8a0be2e5913ee2221819d909b5d2b8ca20c1
Jude Odey <122827182+odey123@users.noreply.github.com>
2024-10-28 14:17:56 +0100

    Update lecturer.js

--- routes/lecturer.js
+++ routes/lecturer.js
@@ -2,19 +2,11 @@ const express = require('express');
 const router = express.Router();
 const lecturer = require('../controllers/lecturer');
 
-// Route to create a course
+
 router.post('/create-course', lecturer.createCourse);
-
-// Route to update course details
-router.patch('/course/:courseId', lecturer.updateCourse);
-
-// Route to delete a course
-router.delete('/course/:courseId', lecturer.deleteCourse);
-
-// Route to view students in a course
-router.get('/course/:courseId/students', lecturer.viewCourseStudents);
-
-// Route to upload materials
-router.post('/course/:courseId/material', lecturer.uploadCourseMaterial);
+router.patch('/update-course/:courseId', lecturer.updateCourse);
+router.delete('/delete-course/:courseId', lecturer.deleteCourse);
+router.get('/view-student/:courseId/students', lecturer.viewCourseStudents);
+router.post('/upload-materials/:courseId/material', lecturer.uploadCourseMaterial);
 
 module.exports = router;
